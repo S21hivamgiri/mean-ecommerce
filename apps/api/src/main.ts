ebd39e067b37c9d1b44e98403cc2a1899e164e57
Shivam Giri <shivamgiri2015@gmail.com>
2026-08-21 19:55:41 +0200

    [BA] Add error-handler middleware

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,9 +2,11 @@ const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3333;
 import { createApp } from './app';
 import { OrdersRouter } from './orders.route';
+import {errorHandler} from "./middleware/error-handler"
 const app = createApp();
 
 app.use('/orders', OrdersRouter);
+app.use(errorHandler);
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
 });
