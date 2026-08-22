4c9f8ff80d87cd195fb86d70b59001b396d535a8
Shivam Giri <shivamgiri2015@gmail.com>
2026-08-22 20:08:45 +0200

    [BA] Add Scaffolding of new Payment Application.

---
+++ apps/payment/src/main.ts
@@ -0,0 +1,38 @@
+import { errorHandler } from './middleware/error-handler';
+import express from 'express';
+
+const host = process.env.HOST ?? 'localhost';
+const port = process.env.PORT ? Number(process.env.PORT) : 3333;
+
+const app = express();
+app.use(express.json());
+
+app.get('/health', (_req, res) => {
+  res.json({
+    status: 'ok',
+  });
+});
+
+app.get('/', (_req, res) => {
+  res.send('Payment Server');
+});
+
+// CORS configuration for React app
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
+app.use(errorHandler);
+app.listen(port, host, () => {
+  console.log(`[ ready ] http://${host}:${port}`);
+});
