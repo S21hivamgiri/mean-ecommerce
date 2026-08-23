200ba53a6c54fb2999443413aa39f7962a850f48
Shivam Giri <shivamgiri2015@gmail.com>
2026-08-23 13:11:56 +0200

    [BA] Add pino logger library with logger config

---
+++ packages/shared/logger/src/lib/logger.ts
@@ -0,0 +1,14 @@
+import pino from 'pino';
+
+export function createLogger(serviceName: string) {
+  return pino({
+    level: process.env.LOG_LEVEL || 'info',
+
+    base: {
+      service: serviceName,
+      environment: process.env.NODE_ENV || 'development',
+    },
+
+    timestamp: pino.stdTimeFunctions.isoTime,
+  });
+}
