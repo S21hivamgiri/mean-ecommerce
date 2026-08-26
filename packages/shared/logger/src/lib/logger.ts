1fed4df7eaf4c7bdb3ae7b9a3c316b022e6ac3a4
Shivam Giri <shivamgiri2015@gmail.com>
2026-08-26 12:18:45 +0200

    [BA] Configue Pino output to setup as log for loki

--- packages/shared/logger/src/lib/logger.ts
+++ packages/shared/logger/src/lib/logger.ts
@@ -1,14 +1,47 @@
 import pino from 'pino';
+import { context, trace } from '@opentelemetry/api';
 
 export function createLogger(serviceName: string) {
-  return pino({
-    level: process.env.LOG_LEVEL || 'info',
-
-    base: {
-      service: serviceName,
-      environment: process.env.NODE_ENV || 'development',
+  const streams = [
+    {
+      stream: process.stdout,
     },
+    {
+      stream: pino.destination({
+        dest: `logs/${serviceName}.log`,
+        mkdir: true,
+        sync: false,
+      }),
+    },
+  ];
 
-    timestamp: pino.stdTimeFunctions.isoTime,
-  });
+  return pino(
+    {
+      level: process.env.LOG_LEVEL || 'info',
+
+      base: {
+        service: serviceName,
+        environment: process.env.NODE_ENV || 'development',
+      },
+
+      timestamp: pino.stdTimeFunctions.isoTime,
+
+      mixin() {
+        const span = trace.getSpan(context.active());
+
+        if (!span) {
+          return {};
+        }
+
+        const spanContext = span.spanContext();
+
+        return {
+          traceId: spanContext.traceId,
+          spanId: spanContext.spanId,
+          traceFlags: spanContext.traceFlags,
+        };
+      },
+    },
+    pino.multistream(streams),
+  );
 }
